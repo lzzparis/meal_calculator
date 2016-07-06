d5b6615c17225dcab8488067467c8f620e767706
Lizzie Paris <lzz.paris@gmail.com>
2016-07-05 21:17:07 -0700

    add bill fields to Diner and DinnerParty objects, create calcSubtotal on Diner

--- meal_calculator.js
+++ meal_calculator.js
@@ -1,22 +1,44 @@
 //Diner constructor
-var Diner = function(){
-	this.meal=[];
+var Diner = function(name) {
+	this.name = name;
+	this.meal = [];
+	this.subtotal = 0;
+	this.tax = 0;
+	this.tip = 0;
+	this.total = 0;
 }
 
-Diner.prototype.addDish = function(dish,cost){
-	this.meal.push({dish:dish, cost:cost});
+Diner.prototype.addDish = function(dish,price) {
+	this.meal.push({dish:dish, price:price});
+}
+Diner.prototype.calcSubtotal = function() {
+	for(var d = 0; d < this.meal.length; d += 1) {
+		this.subtotal += this.meal[d].price;
+	}
 }
 
 //DinnerParty constructor
-//var DinnerParty = function(){
-//	diners:[]
-//}
+var DinnerParty = function() {
+	this.diners = [];
+	this.subtotal = 0;
+	this.tax = 0;
+	this.tip = 0;
+	this.total = 0;
+}
 
-//DinnerParty.prototype.
+DinnerParty.prototype.addDiner = function(diner) {
+	this.diners.push(diner);
+}
 
-var chad = new Diner();
+var party = new DinnerParty();
+var chad = new Diner("Chad");
 
 chad.addDish("potatoes",4.39);
+chad.addDish("mustard",1.69);
+chad.calcSubtotal();
 
-console.log(chad);
+party.addDiner(chad);
+console.log(party);
+
+
 
